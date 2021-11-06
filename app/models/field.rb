c31f91d45bb87f3dfeb8ee0d5f26970a32272e46
pawel150199 <pawel.polski99@gmail.com>
2021-11-06 19:52:41 +0100

    Dodałem validatory

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,4 @@
 class Field < ApplicationRecord
     has_many :questions
+    validates :name, presence: true, length: { in: 50..200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_many :statistics
+    validates :username, presence: true, uniqueness: true , length: { in: 3..20 }
+    validates :last_score, presence: true, length: {in: 0..100}
 end
