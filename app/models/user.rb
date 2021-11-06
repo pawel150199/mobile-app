2a193c5a7cd61cb7d78dafde6a488ff0e0c04910
pawel150199 <pawel.polski99@gmail.com>
2021-11-06 22:01:39 +0100

    Dodano bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :statistics
     validates :username, presence: true, uniqueness: true , length: { in: 3..20 }
     validates :last_score, presence: true, length: {in: 0..100}
+    has_secure_password
 end
